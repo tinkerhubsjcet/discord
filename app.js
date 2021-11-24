8b29169d39c0bf089de28dcb88b3e54d4dc4ca87
Rajat Mathew <rajatxmathew@gmail.com>
2021-11-24 22:42:10 +0530

    upd

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 // Random no generator //
-function randomgenerator(min , max) {
-    let no = Math.random() * max-min +1;
-    no = Math.floor(no) + min;
-    if(no == 1)
-        document.body.style.background = "#215B67";
-    else if(no == 2)
-        document.body.style.background = "#96BF18";
-    else if(no == 3)
-        document.body.style.background = "#EE1702";
-    else if(no == 4)
-        document.body.style.background = "#FECD11";
-    else if(no == 5)
-        document.body.style.background = "#05BFCE";
-    else
-        document.body.style.background = "#0060FF";
+function randomgenerator(min, max) {
+  let no = Math.random() * max - min + 1;
+  no = Math.floor(no) + min;
+  if (no == 1) document.body.style.background = "#215B67";
+  else if (no == 2) document.body.style.background = "#96BF18";
+  else if (no == 3) document.body.style.background = "#EE1702";
+  else if (no == 4) document.body.style.background = "#FECD11";
+  else if (no == 5) document.body.style.background = "#05BFCE";
+  else document.body.style.background = "#0060FF";
 }
 
 function thmain() {
-    window.open("https://tinkerhubsjcet.github.io/linktree/");
+  window.open("https://tinkerhubsjcet.github.io/linktree/");
 }
 
 let btn_submit = document.getElementsByClassName("btn-submit");
-
-function fn() {
-    
-}
